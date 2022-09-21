adeb71558700b7fd8e0100d541b5bf16bf3edb6c
Hao Liu <higurashi-kagome@qq.com>
2022-09-21 14:33:37 +0800

    添加轮询，调整名称、打印方式

--- JavaCardReader/src/main/java/entity/NFCReader.java
+++ JavaCardReader/src/main/java/entity/NFCReader.java
@@ -2,9 +2,9 @@ package entity;
 
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.HexUtil;
-import com.google.gson.Gson;
 import com.sun.jna.Native;
 import api.Declare;
+import org.apache.commons.lang3.StringUtils;
 
 import java.nio.charset.Charset;
 import java.util.regex.Matcher;
@@ -19,7 +19,7 @@ public class NFCReader {
     /**
      * ic 卡标识符
      */
-    private int icDev = 0;
+    private Integer icDev = 0;
     /**
      * 卡序列号
      */
@@ -39,7 +39,7 @@ public class NFCReader {
         if (rf != null) {
             System.out.println("DLL加载成功！");
         } else {
-            System.out.println("DLL加载失败！");
+            System.err.println("DLL加载失败！");
             System.exit(100);
         }
     }
@@ -67,7 +67,7 @@ public class NFCReader {
             String str = new String(version, 0, 15, Charset.forName("GBK"));
             System.out.println("设备初始化成功！硬件版本号：" + str);
         } else {
-            System.out.println("设备连接失败！" + getErrCodeMsg());
+            System.err.println("设备连接失败！" + getErrCodeMsg());
         }
         loadNfcKeys();
     }
@@ -85,7 +85,7 @@ public class NFCReader {
             }
             resultCode = rf.rf_load_key(icDev, (short) 0, i, key);
             if (resultCode != 0) {
-                System.out.println("加载 " + i + " 扇区密码失败！" + getErrCodeMsg());
+                System.err.println("加载 " + i + " 扇区密码失败！" + getErrCodeMsg());
             } else {
                 System.out.println("加载 " + i + " 扇区密码成功！");
             }
@@ -93,57 +93,55 @@ public class NFCReader {
     }
 
     /**
-     * 读取 M1 卡中的数据
+     * 读取卡中的 JSON 字符串
+     * @return 找到返回 JSON 字符串，没找到返回空字符串
      */
-    public void readMifareOne() {
-        byte[] allData = readAllData();
+    public String readJson() {
+        byte[] allData = readAllBytes();
         String s = new String(allData);
         //匹配 json 对象字符串
+        String jsonStr = "";
         Pattern pattern = Pattern.compile("(\\{[^\\{\\}]*\\})");
         Matcher matcher = pattern.matcher(s);
         if (matcher.find()) {
-            String jsonStr = matcher.group(0);
-            Gson gson = new Gson();
-            DataEntity dataEntity = gson.fromJson(jsonStr, DataEntity.class);
-            System.out.println(dataEntity);
-        } else {
-            System.out.println("未知的数据格式");
+            jsonStr = matcher.group(0);
         }
         // 采用ALL模式寻卡（见 findCard 中的 rf_card），结束时执行 rf_halt，避免寻卡总是一次对，一次错
         rf.rf_halt(icDev);
+        return jsonStr;
     }
 
     /**
      * 获取所有可存位内容到 byte 数组
      * @return 读入了所有可存位 byte 数组
      */
-    public byte[] readAllData() {
-        byte[] allData = new byte[0];
+    public byte[] readAllBytes() {
+        byte[] allBytes = new byte[0];
         byte[] rdata = new byte[16];
         int sector = 0;
         resultCode = rf.rf_authentication(icDev, (short) 0, (short) sector);
         if (resultCode != 0) {
-            System.out.println(sector + "扇区密码验证错误！" + getErrCodeMsg());
+            System.err.println(sector + "扇区密码验证错误！" + getErrCodeMsg());
         }
         for (int i = 1; i < 64; i++) {
             if ((i + 1) % 4 == 1) { // 每进入一个新的扇区，都要进行验证
                 sector++;
                 resultCode = rf.rf_authentication(icDev, (short) 0, (short) sector);
                 if (resultCode != 0) {
-                    System.out.println(sector + "扇区密码验证错误！" + getErrCodeMsg());
+                    System.err.println(sector + "扇区密码验证错误！" + getErrCodeMsg());
                 }
             }
             if ((i + 1) % 4 != 0) { // 每个扇区的第四块为特殊块，不存数据
                 resultCode = rf.rf_read(icDev, (short) i, rdata);
                 if (resultCode == 0) {
                     System.out.println(HexUtil.encodeHex(rdata));
-                    allData = ArrayUtil.addAll(allData, rdata);
+                    allBytes = ArrayUtil.addAll(allBytes, rdata);
                 } else {
-                    System.out.println("读数据失败！块：" + i);
+                    System.err.println("读数据失败！块：" + i);
                 }
             }
         }
-        return allData;
+        return allBytes;
     }
 
     /**
@@ -155,9 +153,9 @@ public class NFCReader {
     }
 
     /**
-     * 寻卡，寻卡后卡序列号写入 {@link snr}
+     * 寻卡，寻卡后卡序列号写入 snr
      */
-    public void findCard() {
+    public int findCard() {
         // 采用ALL模式寻卡，结束时执行 rf_halt(见 readMifareOne 中的 rf_halt)，避免寻卡总是一次对，一次错
         resultCode = rf.rf_card(icDev, (short) 1, snr);
         if (resultCode == 0) {
@@ -166,16 +164,47 @@ public class NFCReader {
             String SnrStr = new String(snrHex, 0, 8);
             System.out.println("寻卡成功，卡序列号：" + SnrStr);
         } else {
-            System.out.println("寻卡失败！" + getErrCodeMsg());
+            System.err.println("寻卡失败！" + getErrCodeMsg());
+        }
+        return resultCode;
+    }
+
+    /**
+     * 蜂鸣
+     * @param timeout 蜂鸣时长，单位毫秒
+     */
+    public void beep(int timeout) {
+        if (timeout <= 0) return;
+        if (icDev != null) {
+            // rf_beep timeout 单位是10毫秒
+            short beepTimout = (short) (timeout * 10);
+            resultCode = rf.rf_beep(icDev, beepTimout);
         }
     }
 
-    public static void main(String[] args) {
+    /**
+     * TODO：多卡同时放入的问题
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
         // rf32.dll 位于 jna jar 包根目录
         NFCReader con = new NFCReader();
         con.connect();
-        con.findCard();
-		con.readMifareOne();
+        for (int i = 0; i < 20; i++) {
+            int resultCode = con.findCard();
+            if (resultCode == 0) {
+                String jsonStr = con.readJson();
+                if (StringUtils.isBlank(jsonStr)) {
+                    System.out.println(jsonStr);
+                    con.beep(50);
+                } else {
+                    System.err.println("未找到 JSON 字符串");
+                }
+            } else {
+                Thread.sleep(500);
+            }
+        }
 		con.disconnect();
     }
 }
